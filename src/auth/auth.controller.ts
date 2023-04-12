1936e122f2f086a67621e349c000f4bcae2b7175
Diego ramirez <diego.ramirez@kubide.es>
2023-04-12 14:18:58 +0200

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+
+  @Get('signuot')
+  signuot() {
+    return this.authService.signout();
+  }
+
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+    constructor() {}
+
+    async signup() {
+        return { message : 'signup was succefull' };
+    }
+    async signin() {
+        return '';
+    }
+    async signout() {
+        return '';
+    }
+}
