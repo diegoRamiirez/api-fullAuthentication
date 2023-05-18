68978736498de2ad667643b534e91bd7926113cb
Diego ramirez <diego.ramirez@kubide.es>
2023-05-18 16:26:26 +0200

    fix strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
                 JwtStrategy.extractJWT,
-
             ]),
             secretOrKey: jwtSecret,
         })
