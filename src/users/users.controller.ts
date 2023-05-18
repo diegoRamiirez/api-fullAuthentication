d25f7993cbb18996e9ea272e0074235d16618969
Diego ramirez <diego.ramirez@kubide.es>
2023-05-18 15:10:28 +0200

    install , and create jwt guard , strategy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
+
+  @Get('id')
+  getMyUser(@Param() params: { id: string }) {
+    return this.usersService.getMyUser(params.id);
+  }
+
+  @Get()
+  getUsers() {
+   return this.usersService.getUsers();
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) { }
+
+    async getMyUser(id: string) {
+
+    }
+
+    async getUsers() {
+        return await this.prisma.user.findMany({
+            select: { id: true, email: true },
+        });
+    }
+}
